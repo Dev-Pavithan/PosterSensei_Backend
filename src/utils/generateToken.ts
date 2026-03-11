883d5f53736f70d90ed3736f7dbd60e87bc18da2
Unenthiran Pavithan <pavithanunenthiran29@gmail.com>
2026-03-12 00:42:29 +0530

    fix: allow cross-domain auth cookies for Vercel deployment

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -8,8 +8,8 @@ const generateToken = (res: Response, userId: string) => {
 
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
+        secure: true, // Must be true for sameSite: 'none'
+        sameSite: 'none',
         maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 };
